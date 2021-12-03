ed7a3a8b267cf8a370b0f83b5f1c096a30af2a69
Tim <timothy_may@hotmail.co.uk>
2021-12-03 18:27:50 +0000

    Completed challenge two

--- day-one/day-one.hs
+++ day-one/day-one.hs
@@ -13,7 +13,18 @@ countIncreases (curr:next:rest)
         | otherwise = countIncreases ([next] ++ rest)
 
 
+countIncreasesSliding :: [Int] -> Int
+countIncreasesSliding [] = 0
+countIncreasesSliding [a] = 0
+countIncreasesSliding [a, b] = 0
+countIncreasesSliding [a, b, c] = 0
+countIncreasesSliding (a:b:c:d:rest)
+        | b + c + d > a + b + c = 1 + countIncreasesSliding ([b] ++ [c] ++ [d] ++ rest)
+        | otherwise = countIncreasesSliding ([b] ++ [c] ++ [d] ++ rest)
+
+
 main = do  
         contents <- readFile "data.txt"
         let list :: [Int] = map toInt . words $ contents
         print $ countIncreases list
+        print $ countIncreasesSliding list
