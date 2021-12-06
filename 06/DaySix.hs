2ce32b4a018ac45c5d96c663c06f1549ca7e5504
Tim <timothy_may@hotmail.co.uk>
2021-12-06 18:04:48 +0000

    Refactored code to use School name instead of Timer for clarity

--- 06/DaySix.hs
+++ 06/DaySix.hs
@@ -8,58 +8,58 @@ reproductionTime :: Int = 6
 newFishReproductionTime :: Int = 8
 
 
-data Timer = Timer {daysLeft :: Int, count :: Int} deriving (Show, Ord, Eq)
+data School = School {daysLeft :: Int, count :: Int} deriving (Show, Eq, Ord)
 
 
-prepareData :: String -> [Timer]
+prepareData :: String -> [School]
 prepareData dataString =
     let 
         characters = words $ map (\c -> if c == ',' then ' ' else c) dataString
-        createTimers l = Timer (head l) (length l)
+        createTimers l = School (head l) (length l)
     in  map createTimers $ group $ sort $ map (read) characters
 
 
-simulateDay :: [Timer] -> [Timer]
+simulateDay :: [School] -> [School]
 simulateDay [] = []
-simulateDay timers@(t:ts)
-    | d == 0 = [Timer reproductionTime c] ++ [Timer newFishReproductionTime c] ++ simulateDay ts
-    | otherwise = [Timer (d - 1) c] ++ simulateDay ts
+simulateDay schools@(s:rest)
+    | d == 0 = [School reproductionTime c] ++ [School newFishReproductionTime c] ++ simulateDay rest
+    | otherwise = [School (d - 1) c] ++ simulateDay rest
     where
-        d = daysLeft t
-        c = count t
+        d = daysLeft s
+        c = count s
 
 
-sortTimers :: [Timer] -> [Timer]
-sortTimers timers = sortBy (comparing daysLeft) timers
+sortSchools :: [School] -> [School]
+sortSchools schools = sortBy (comparing daysLeft) schools
 
 
-groupTimers :: [Timer] -> [Timer]
-groupTimers [] = []
-groupTimers (t0:t1:ts)
-    | d0 == d1 = [Timer d0 (c0 + c1)] ++ ts  -- group same day counts together
-    | otherwise = [t0] ++ [t1] ++ ts
+groupSchools :: [School] -> [School]
+groupSchools [] = []
+groupSchools (s0:s1:rest)
+    | d0 == d1 = [School d0 (c0 + c1)] ++ rest  -- group same day counts together
+    | otherwise = [s0] ++ [s1] ++ rest
     where
-        d0 = daysLeft t0
-        d1 = daysLeft t1
-        c0 = count t0
-        c1 = count t1
+        d0 = daysLeft s0
+        d1 = daysLeft s1
+        c0 = count s0
+        c1 = count s1
 
 
-runSimulation :: [Timer] -> Int -> [Timer]
+runSimulation :: [School] -> Int -> [School]
 runSimulation [] _ = []
-runSimulation timers 0 = timers
-runSimulation timers days
-    | days == 0 = newTimers
-    | otherwise = runSimulation newTimers (days - 1)
-    where newTimers = groupTimers$ sortTimers $ simulateDay timers
+runSimulation schools 0 = schools
+runSimulation schools days
+    | days == 0 = newSchools
+    | otherwise = runSimulation newSchools (days - 1)
+    where newSchools = groupSchools $ sortSchools $ simulateDay schools
 
 
-countFish :: [Timer] -> Int
-countFish timers = sum $ map (count) timers
+countFish :: [School] -> Int
+countFish schools = sum $ map (count) schools
 
 
 main = do
     contents <- readFile "data.txt"
-    let initialPop :: [Timer] = prepareData contents
-    let testData :: [Timer] = prepareData "3,4,3,1,2"
+    let initialPop :: [School] = prepareData contents
+    let testData :: [School] = prepareData "3,4,3,1,2"
     print $ countFish $ runSimulation initialPop 256
